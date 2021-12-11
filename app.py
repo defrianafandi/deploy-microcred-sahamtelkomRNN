eeaa184900a50f79bf4ad5519cfc2ca4a1f74b72
defrianafandi <defrianafandi@gmail.com>
2021-12-11 08:32:01 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+model = load_model('model.h5')
+
+@app.route('/')
+def index():
+    return render_template('index.html', predict_close=0)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    Mengambil nilai close dan predict ke model
+    '''
+    close = request.form.values()
+    prediction = model.predict(close)
+
+    return render_template('index.html', predict_close=prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
